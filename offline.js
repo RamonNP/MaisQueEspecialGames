2f1570e0e4921330e29f20f4ef16722ae3a45b7f
Ramon Pereira <ramon.pereira3156@gmail.com>
2019-07-14 19:26:38 -0300

    Commit inicial

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1563143071,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bakgroundloading-sheet0.png",
+		"images/loading-sheet0.png",
+		"images/long-sheet0.png",
+		"images/queespecial-sheet0.png",
+		"images/j-sheet0.png",
+		"images/j-sheet1.png",
+		"images/l1_background-sheet0.png",
+		"images/l2_mountains-sheet0.png",
+		"images/l3_clouds-sheet0.png",
+		"images/l4_bushes-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
